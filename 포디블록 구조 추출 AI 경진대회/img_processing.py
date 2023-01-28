0f53d5d85b638bdefdd24121b9e942825d86f99b
parkchanghyup <57655448+parkchanghyup@users.noreply.github.com>
2023-01-28 18:14:02 +0900

    Update img_processing.py

--- 포디블록 구조 추출 AI 경진대회/img_processing.py
+++ 포디블록 구조 추출 AI 경진대회/img_processing.py
@@ -5,15 +5,29 @@ from rembg import remove
 import tqdm
 
 
-train_path  = glob.glob('./train/*')
-background_path = glob.glob('./background/*')
+test_path  = glob.glob('/Users/harvey/Downloads/open/test/*')
+train_path  = glob.glob('/Users/harvey/Downloads/open/train/*')
+test_path.sort()
+train_path.sort()
+background_path = glob.glob('/Users/harvey/Downloads/background/*')
 
 
-for path in tqdm.tqdm(train_path):
+
+
+for idx,  path in tqdm.tqdm(enumerate(test_path)):
+    img = Image.open(path)
+    img_name = './test_2/'+path.split('/')[-1]
+    background = [Image.open(img_path).resize((400, 400)) for img_path in background_path]
+    background_img = background[idx%9]
+    rem_img_mask = remove(img, only_mask = True)
+    background_img.paste(img, mask = rem_img_mask)
+    background_img.save(img_name)
+
+for idx,  path in tqdm.tqdm(enumerate(train_path)):
     img = Image.open(path)
     img_name = './train_2/'+path.split('/')[-1]
     background = [Image.open(img_path).resize((400, 400)) for img_path in background_path]
-    background_img = background[random.randint(0, len(background)-1)]
-    rem_img_mask = remove(img, only_mask=True)
-    background_img.paste(img, mask=rem_img_mask)
+    background_img = background[idx%9]
+    rem_img_mask = remove(img, only_mask = True)
+    background_img.paste(img, mask = rem_img_mask)
     background_img.save(img_name)
